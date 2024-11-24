f142cf713e28a78db402e4b2f4210ed3f7734961
Jhonatas <jhonatas.sil.generoso@gmail.com>
2024-11-24 13:26:39 -0300

    Work in progress, fetch pet data

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 async function start() {
-    const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast");
-    const weatherData = await weatherPromise.json();
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast");
+  const weatherData = await weatherPromise.json();
 
-    const ourTemperature = weatherData.properties.periods[0].temperature;
-    document.querySelector("#temperature-output").textContent = ourTemperature;
+  const ourTemperature = weatherData.properties.periods[0].temperature;
+  document.querySelector("#temperature-output").textContent = ourTemperature;
 }
 
 start();
+
+async function petsArea() {
+  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json");
+  const petsData = await petsPromise.json();
+  petsData.forEach(pet => {
+    console.log(pet.name);
+  });
+}
+
+petsArea();
