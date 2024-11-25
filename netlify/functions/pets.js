0b6c2e02976083e91a847ce37407808bfa29a069
Jhonatas <jhonatas.sil.generoso@gmail.com>
2024-11-25 00:26:36 -0300

    Revert "First commit"
    This reverts commit 03d0163a44dac8fb68dc9f2aacfb4b3c1d2f01a3.

--- netlify/functions/pets.js
+++
@@ -1,9 +0,0 @@
-const handler = async () => {
-    return {
-        statusCode: 200,
-        headers: {"Content-Type": "text/plain"},
-        body: 2 + 2
-    }
-};
-
-module.exports = {handler};
